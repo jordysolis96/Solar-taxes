29114c7aabb963d5db0724e5a0883f34e3d022ed
jordy muniz <jordy.muniz96@gmail.com>
2021-02-11 18:53:59 -0600

    added git ignore

---
+++ src/main/java/com/taxes/solartaxes/SolarTaxesApplication.java
@@ -0,0 +1,13 @@
+package com.taxes.solartaxes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarTaxesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolarTaxesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taxes/solartaxes/SolarTaxesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taxes.solartaxes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarTaxesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

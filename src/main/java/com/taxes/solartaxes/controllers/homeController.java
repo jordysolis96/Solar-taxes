90a6dc8f7b4e0a78ed959362f7806f8a651ab807
jordy muniz <jordy.muniz96@gmail.com>
2021-02-25 12:23:46 -0600

    added simple index page

---
+++ src/main/java/com/taxes/solartaxes/controllers/homeController.java
@@ -0,0 +1,13 @@
+package com.taxes.solartaxes.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homeController {
+
+    @GetMapping("/")
+    public String homeView() {
+        return "Index";
+    }
+}
